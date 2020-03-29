9d43a4ccddce10a301b6ddf85ae7ad30e55d1d82
neko3cs <minaton.0722.net@gmail.com>
2020-03-29 22:54:49 +0900

    パスワードが丸見えなの修正なの

--- DnZip/Program.cs
+++ DnZip/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using Cocona;
 using ICSharpCode.SharpZipLib.Zip;
 
@@ -27,23 +28,17 @@ namespace DnZip
                 return 1;
             }
 
-            var password = string.Empty;
+            var zip = new FastZip();
             if (encrypt)
             {
-                Console.Write("Enter password: ");
-                var pw1 = Console.ReadLine();
-                Console.Write("Verify password: ");
-                var pw2 = Console.ReadLine();
-                if (!pw1.Equals(pw2))
+                var password = GetPasswordFromConsole();
+                if (string.IsNullOrEmpty(password))
                 {
                     Console.WriteLine("Error: Password verification failed.");
                     return 1;
                 }
-                password = pw1;
+                zip.Password = password;
             }
-
-            var zip = new FastZip();
-            if (encrypt) zip.Password = password;
             zip.CreateZip(
                 zipFileName: Path.Combine(targetDirectory.Parent.FullName, $"{targetDirectory.Name}.zip"),
                 targetDirectory.FullName,
@@ -53,6 +48,71 @@ namespace DnZip
             );
 
             return 0;
+        }
+
+        private string GetPasswordFromConsole()
+        {
+            Console.Write("Enter password: ");
+            var pw1 = ConsoleReadPassword();
+            Console.Write("Verify password: ");
+            var pw2 = ConsoleReadPassword();
+            if (!pw1.Equals(pw2))
+            {
+                return string.Empty;
+            }
+            return pw1;
+        }
+
+        private string ConsoleReadPassword()
+        {
+            var password = new StringBuilder();
+
+            while (true)
+            {
+                var keyinfo = Console.ReadKey(intercept: true);
+
+                if (keyinfo.Key.Equals(ConsoleKey.Enter))
+                {
+                    Console.WriteLine();
+                    return password.ToString();
+                }
+                else if (keyinfo.Key.Equals(ConsoleKey.Backspace))
+                {
+                    if (password.Length > 0)
+                    {
+                        password.Length -= 1;
+                        continue;
+                    }
+                }
+                else if (Char.IsLetter(keyinfo.KeyChar))
+                {
+                    if ((keyinfo.Modifiers & ConsoleModifiers.Shift) == 0)
+                    {
+                        password.Append(keyinfo.KeyChar);
+                        continue;
+                    }
+                    else
+                    {
+                        if (Console.CapsLock)
+                        {
+                            password.Append(Char.ToLower(keyinfo.KeyChar));
+                            continue;
+                        }
+                        else
+                        {
+                            password.Append(Char.ToUpper(keyinfo.KeyChar));
+                            continue;
+                        }
+                    }
+                }
+                else if (!Char.IsControl(keyinfo.KeyChar))
+                {
+                    password.Append(keyinfo.KeyChar);
+                    continue;
+                }
+
+                Console.Beep();
+            }
         }
     }
 }
