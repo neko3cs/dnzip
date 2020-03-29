313285619d205d883d3b4afc68e5227e23e16b6e
neko3cs <minaton.0722.net@gmail.com>
2020-03-29 14:33:19 +0900

    もっと楽な実装があったので置換え

--- DnZip/Program.cs
+++ DnZip/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using Cocona;
 using ICSharpCode.SharpZipLib.Zip;
@@ -10,9 +9,6 @@ namespace DnZip
     {
         static void Main(string[] args)
         {
-            // TODO: 1: 無階層で zip 出来る機能の実装
-            // TODO: 2: 1 に暗号化機能を実装
-            // TODO: 3: フォルダ階層を維持して zip 出来る機能の実装
             CoconaLiteApp.Run<Program>(args);
         }
 
@@ -23,8 +19,8 @@ namespace DnZip
         )
         {
             if (string.IsNullOrEmpty(path)) return 1;
-            var zipDir = new DirectoryInfo(path);
-            if (!zipDir.Exists)
+            var targetDirectory = new DirectoryInfo(path);
+            if (!targetDirectory.Exists)
             {
                 Console.WriteLine("Error: Path not found.");
                 return 1;
@@ -34,45 +30,26 @@ namespace DnZip
             if (encrypt)
             {
                 Console.Write("Enter password: ");
-                var p1 = Console.ReadLine();
+                var pw1 = Console.ReadLine();
                 Console.Write("Verify password: ");
-                var p2 = Console.ReadLine();
-                if (!p1.Equals(p2))
+                var pw2 = Console.ReadLine();
+                if (!pw1.Equals(pw2))
                 {
                     Console.WriteLine("Error: Password verification failed.");
                     return 1;
                 }
+                password = pw1;
             }
 
-            using (var memoryStream = new MemoryStream())
-            using (var zipOutputStream = new ZipOutputStream(memoryStream))
-            {
-                zipOutputStream.SetLevel(9);
-                var buffer = new byte[4096];
-
-                foreach (var file in zipDir.GetFiles())
-                {
-                    var entry = new ZipEntry(file.Name);
-                    entry.DateTime = DateTime.Now;
-                    zipOutputStream.PutNextEntry(entry);
-
-                    using (var fileStream = File.OpenRead(file.FullName))
-                    {
-                        var sourceBytes = 0;
-                        do
-                        {
-                            sourceBytes = fileStream.Read(buffer, 0, buffer.Length);
-                            zipOutputStream.Write(buffer, 0, sourceBytes);
-                        } while (sourceBytes > 0);
-                    }
-                }
-
-                zipOutputStream.Finish();
-                zipOutputStream.Close();
-
-                var zipFileFullName = Path.Combine(zipDir.Parent.FullName, $"{zipDir.Name}.zip");
-                File.WriteAllBytes(zipFileFullName, memoryStream.ToArray());
-            }
+            var zip = new FastZip();
+            if (encrypt) zip.Password = password;
+            zip.CreateZip(
+                zipFileName: Path.Combine(targetDirectory.Parent.FullName, $"{targetDirectory.Name}.zip"),
+                targetDirectory.FullName,
+                recurse: true,
+                fileFilter: null,
+                directoryFilter: null
+            );
 
             return 0;
         }
