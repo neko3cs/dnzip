008dcf00ac8dcd078138a12c47101d745d47b83b
neko3cs <minaton.0722.net@gmail.com>
2020-05-31 12:44:32 +0900

    FastZip やめてみた（でもアーカイブ出来ない）

--- DnZip/Program.cs
+++ DnZip/Program.cs
@@ -2,10 +2,13 @@
 using System.IO;
 using System.Text;
 using Cocona;
+using ICSharpCode.SharpZipLib.Checksum;
+using ICSharpCode.SharpZipLib.Core;
 using ICSharpCode.SharpZipLib.Zip;
 
 namespace DnZip
 {
+    // HACK: 複数ファイル指定に対応
     class Program
     {
         static void Main(string[] args)
@@ -15,54 +18,53 @@ namespace DnZip
 
         [PrimaryCommand]
         public int CompressZipFile(
-            [Argument]string path,
-            [Option('r')]bool recursePaths,
-            [Option('e')]bool encrypt
+            [Argument] string archiveFilePath,
+            [Argument] string sourceDirectoryPath,
+            [Option('r')] bool recurse,
+            [Option('e')] bool encrypt
         )
         {
-            if (string.IsNullOrEmpty(path)) return 1;
-            var targetDirectory = new DirectoryInfo(path);
-            if (!targetDirectory.Exists)
+            if (string.IsNullOrEmpty(archiveFilePath)) return 1;
+            if (string.IsNullOrEmpty(sourceDirectoryPath)) return 1;
+            var sourceDirectory = new DirectoryInfo(sourceDirectoryPath);
+            if (!sourceDirectory.Exists)
             {
-                Console.WriteLine("Error: Path not found.");
+                Console.WriteLine("Error: Source Directory path not found.");
                 return 1;
             }
 
-            var zip = new FastZip();
+            var password = string.Empty;
             if (encrypt)
             {
-                var password = GetPasswordFromConsole();
-                if (string.IsNullOrEmpty(password))
+                if (!TryGetPasswordFromConsole(out password))
                 {
                     Console.WriteLine("Error: Password verification failed.");
                     return 1;
                 }
-                zip.Password = password;
             }
-            // FIXME: 対象ディレクトリの中身だけが書庫化されるため、解凍すると中身が散らばる
-            // HACK: 複数ファイル指定に対応
-            zip.CreateZip(
-                zipFileName: Path.Combine(targetDirectory.Parent.FullName, $"{targetDirectory.Name}.zip"),
-                targetDirectory.FullName,
-                recurse: recursePaths,
-                fileFilter: null,
-                directoryFilter: null
-            );
+
+            // FIXME: macOS, Windows 共に標準のアーカイバーで解凍が出来ない
+            // FIXME: 1 エントリーしかアーカイブされない(macOS, unzip)
+            // FIXME: アーカイブしても空のフォルダしか生成されない(Windows10, Expand-Archive)
+            var archiveFile = new FileInfo(archiveFilePath);
+            CreateArchive(archiveFile, sourceDirectory, recurse, password);
 
             return 0;
         }
 
-        private string GetPasswordFromConsole()
+        private bool TryGetPasswordFromConsole(out string password)
         {
+            password = string.Empty;
+
             Console.Write("Enter password: ");
             var pw1 = ConsoleReadPassword();
             Console.Write("Verify password: ");
             var pw2 = ConsoleReadPassword();
-            if (!pw1.Equals(pw2))
-            {
-                return string.Empty;
-            }
-            return pw1;
+
+            if (!pw1.Equals(pw2)) return false;
+
+            password = pw1;
+            return true;
         }
 
         private string ConsoleReadPassword()
@@ -114,6 +116,64 @@ namespace DnZip
                 }
 
                 Console.Beep();
+            }
+        }
+
+        private void CreateArchive(
+            FileInfo archiveFile,
+            DirectoryInfo sourceDirectory,
+            bool recurse,
+            string password
+        )
+        {
+            using (FileStream outputFileStream = File.Create(archiveFile.FullName))
+            using (var zipOutputStream = new ZipOutputStream(outputFileStream))
+            {
+                // zipOutputStream.UseZip64 = UseZip64.Off;
+                zipOutputStream.SetLevel(9);
+                if (!string.IsNullOrEmpty(password)) zipOutputStream.Password = password;
+                var crc = new Crc32();
+                CompressDirectory(zipOutputStream, sourceDirectory, crc, recurse);
+            }
+        }
+
+        private void CompressDirectory(
+            ZipOutputStream zipOutputStream,
+            DirectoryInfo directory,
+            Crc32 crc,
+            bool recurse
+        )
+        {
+            foreach (var file in directory.GetFiles())
+            {
+                var entry = new ZipEntry(
+                    ZipEntry.CleanName(file.Name)
+                )
+                {
+                    DateTime = file.LastWriteTime,
+                    Size = file.Length
+                };
+                // var bufferForCrc = File.ReadAllBytes(file.FullName);
+                // crc.Reset();
+                // crc.Update(bufferForCrc);
+                // entry.Crc = crc.Value;
+                zipOutputStream.PutNextEntry(entry);
+                // zipOutputStream.Write(bufferForCrc, 0, bufferForCrc.Length);
+
+                var bufferForCopy = new byte[4096];
+                using (FileStream inputFileStream = File.OpenRead(file.FullName))
+                {
+                    StreamUtils.Copy(inputFileStream, zipOutputStream, bufferForCopy);
+                }
+                zipOutputStream.CloseEntry();
+            }
+
+            if (recurse)
+            {
+                foreach (var subDirectory in directory.GetDirectories())
+                {
+                    CompressDirectory(zipOutputStream, subDirectory, crc, recurse);
+                }
             }
         }
     }
