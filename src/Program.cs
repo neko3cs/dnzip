80320f208cff72e89e92bacb1f722af446c96a8d
neko3cs <minaton.0722.net@gmail.com>
2026-01-21 00:40:42 +0900

    クラス分割の廃止

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Threading.Tasks;
 using Sharprompt;
 using ConsoleAppFramework;
+using System.Text;
+using Ionic.Zip;
 
 namespace DnZip
 {
@@ -10,6 +12,11 @@ namespace DnZip
   // HACK: --no-dir-entries(-D) に対応
   public class Program
   {
+    public Program()
+    {
+      Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    }
+
     static async Task Main(string[] args) => await ConsoleApp.RunAsync(args, Compress);
 
     public static int Compress(
@@ -38,7 +45,7 @@ namespace DnZip
       }
       try
       {
-        ZipArchiver.CreateArchive(new FileInfo(archiveFilePath), sourceDirectory, recurse, password);
+        CreateArchive(new FileInfo(archiveFilePath), sourceDirectory, recurse, password);
       }
       catch (Exception e)
       {
@@ -57,6 +64,46 @@ namespace DnZip
 
       password = pw1;
       return true;
+    }
+
+    public static void CreateArchive(
+              FileInfo archiveFile,
+              DirectoryInfo sourceDirectory,
+              bool recursePaths,
+              string password
+          )
+    {
+      using var zip = new ZipFile(Encoding.GetEncoding("Shift_JIS"))
+      {
+        CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression
+      };
+      if (!string.IsNullOrEmpty(password)) zip.Password = password;
+      AddEntry(zip, sourceDirectory, sourceDirectory, recursePaths);
+      zip.Save(archiveFile.FullName);
+    }
+
+    private static void AddEntry(
+        ZipFile zip,
+        DirectoryInfo root,
+        DirectoryInfo target,
+        bool recursePaths
+    )
+    {
+      foreach (var file in target.GetFiles())
+      {
+        var entryPath = Path.GetRelativePath(root.FullName, file.FullName);
+        zip.AddFile(file.FullName, Path.GetDirectoryName(entryPath));
+      }
+
+      if (!recursePaths) return;
+
+      foreach (var subDir in target.GetDirectories())
+      {
+        if (subDir.GetFiles().Length == 0)
+          zip.AddDirectory(subDir.FullName, subDir.Name);
+
+        AddEntry(zip, root, subDir, recursePaths);
+      }
     }
   }
 }
