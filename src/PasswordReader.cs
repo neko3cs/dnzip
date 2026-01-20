367505333a706ef805309cbe1217fb1eb5bb4aec
neko3cs <minaton.0722.net@gmail.com>
2026-01-21 00:02:41 +0900

    クラス分割

---
+++ src/PasswordReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace DnZip;
+
+public class PasswordReader
+{
+  public bool TryGetPasswordFromConsole(out string password)
+  {
+    password = string.Empty;
+
+    Console.Write("Enter password: ");
+    var pw1 = ConsoleReadPassword();
+    Console.Write("Verify password: ");
+    var pw2 = ConsoleReadPassword();
+
+    if (!pw1.Equals(pw2)) return false;
+
+    password = pw1;
+    return true;
+  }
+
+  private string ConsoleReadPassword()
+  {
+    var password = new StringBuilder();
+
+    while (true)
+    {
+      var keyinfo = Console.ReadKey(intercept: true);
+
+      if (keyinfo.Key.Equals(ConsoleKey.Enter))
+      {
+        Console.WriteLine();
+        return password.ToString();
+      }
+      else if (keyinfo.Key.Equals(ConsoleKey.Backspace))
+      {
+        if (password.Length > 0)
+        {
+          password.Length -= 1;
+          continue;
+        }
+      }
+      else if (Char.IsLetter(keyinfo.KeyChar))
+      {
+        if ((keyinfo.Modifiers & ConsoleModifiers.Shift) == 0)
+        {
+          password.Append(keyinfo.KeyChar);
+          continue;
+        }
+        else
+        {
+          if (Console.CapsLock)
+          {
+            password.Append(Char.ToLower(keyinfo.KeyChar));
+            continue;
+          }
+          else
+          {
+            password.Append(Char.ToUpper(keyinfo.KeyChar));
+            continue;
+          }
+        }
+      }
+      else if (!Char.IsControl(keyinfo.KeyChar))
+      {
+        password.Append(keyinfo.KeyChar);
+        continue;
+      }
+
+      Console.Beep();
+    }
+  }
+}
---
+++ src/ZipArchiver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Ionic.Zip;
+
+namespace DnZip;
+
+public class ZipArchiver
+{
+  public void CreateArchive(
+              FileInfo archiveFile,
+              DirectoryInfo sourceDirectory,
+              bool recursePaths,
+              string password
+          )
+  {
+    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    using (var zip = new ZipFile(Encoding.GetEncoding("Shift_JIS")))
+    {
+      zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression;
+      if (!string.IsNullOrEmpty(password)) zip.Password = password;
+      AddEntry(zip, sourceDirectory, sourceDirectory, recursePaths);
+      zip.Save(archiveFile.FullName);
+    }
+  }
+
+  private void AddEntry(
+      ZipFile zip,
+      DirectoryInfo rootDir,
+      DirectoryInfo targetDir,
+      bool recursePaths
+  )
+  {
+    foreach (var file in targetDir.GetFiles())
+    {
+      zip.AddFile(
+          fileName: file.FullName,
+          directoryPathInArchive: targetDir.FullName.Substring(rootDir.FullName.Length)
+      );
+    }
+    if (recursePaths)
+    {
+      foreach (var subDir in targetDir.GetDirectories())
+      {
+        if (!subDir.GetFiles().Any())
+        {
+          zip.AddDirectory(subDir.FullName, subDir.Name);
+        }
+        AddEntry(zip, rootDir, subDir, recursePaths);
+      }
+    }
+  }
+}
